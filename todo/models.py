b9961a091ac1866bdfb31f9827410c69d896e9d3
schash1 <schash1@mail.ru>
2023-02-27 16:24:59 +0300

    first commit

---
+++ todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField.unique
