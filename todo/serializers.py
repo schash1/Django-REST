7794823ae930cba75ef43919c2d124a73256d6a3
Sonya <schash1@mail.ru>
2023-04-05 13:16:50 +0300

    Done

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,4 +5,10 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'login', 'password')
+
+
+class UserFullModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
